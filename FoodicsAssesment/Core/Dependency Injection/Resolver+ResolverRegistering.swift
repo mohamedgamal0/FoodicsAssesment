3312d0f923faf9b8885c1e5c0ed1528604ae75ee
MoGamal <bb>
2020-12-17 10:14:49 +0200

    add Demo Setups
    - I categorized the files and started put the architecture
    - added config file
    - added dependency injection through resolver and internet manger

---
+++ FoodicsAssesment/Core/Dependency Injection/Resolver+ResolverRegistering.swift
@@ -0,0 +1,14 @@
+//
+//  Resolver+ResolverRegistering.swift
+//  FoodicsAssesment
+//
+//  Created by mohamed gamal on 12/17/20.
+//
+
+import Foundation
+
+extension Resolver: ResolverRegistering {
+    
+    public static func registerAllServices() {
+    }
+}
